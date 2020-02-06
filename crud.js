b0dc44103869c01c0f4fa24c09e6c9b271dbaa11
ashishfatnani <40388618+ashishfatnani@users.noreply.github.com>
2020-02-06 14:58:44 +0530

    Add files via upload

---
+++ crud.js
@@ -0,0 +1,85 @@
+//The storage of employees.
+let emp =
+{
+       Ashish: {
+              name: "Ashish",
+              email: "ashish@gmail.com",
+              age: 21,
+              gender: "M",
+              id: 1
+       },
+       Suketu: {
+              name: "Suketu",
+              email: "suketuashkajhk@gmail.com",
+              age: 21,
+              gender: "M"
+       }
+}
+
+// function for validations
+const isValid = (userName, name, email, age, gender) => {
+       if (emp.hasOwnProperty(userName)) {
+              console.error("The username already exist please try other username");  //validates username.
+              return false;
+       }
+       else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) &&
+              /^[a-zA-Z]+(([a-zA-Z ])?[a-zA-Z]*)*$/.test(name) &&                 //validate name,email,age & gender.
+              (age < 100 && age >= 18) && (gender === "M" || gender === "F")) {
+              return true;
+       }
+       else {
+              console.error("Unable to create , Input error");       //prints error.
+              return false;
+       }
+
+}
+
+
+// To display the employee details
+
+const display = (xyz) => {
+       console.log(emp[xyz]);
+}
+
+// To create new employee
+
+const create = (userName, Name, email, age, gender) => {
+       if (isValid(userName, Name, email, age, gender)) {
+              emp[userName] = {
+                     name: Name,
+                     email: email,
+                     age: age,
+                     gender: gender
+              }
+
+       }
+       else {
+
+       }
+
+}
+
+// To update the details for current employees.
+
+const update = (userName, key, value) => {
+       emp[userName][key] = value
+       console.log(`Now ${key} is ${value}`);
+}
+
+//To delete the current employee.
+
+const del = (userName) => {
+       delete emp[userName];
+}
+
+//calling the functions.
+// create("Ashish", "Mustu", "aha@gmail.com", 22, "M");
+// console.log(emp);
+// display("Suketu");
+
+// display("Mustafa");
+// console.log(Object.keys(emp));
+update("Ashish", "age", 22);
+display("Ashish");
+// del("Ashish");
+// console.log(Object.keys(emp));
